c9180f24359efead669f96e0e1368001c6aecce2
sofhiautaka <sofhia.utaka@sptech.school>
2025-06-08 19:31:08 -0300

    mandando arquivo csv ao invés de json

--- webScrap-clima-tempo/webScrap-clima-tempo.py
+++ webScrap-clima-tempo/webScrap-clima-tempo.py
@@ -1,10 +1,11 @@
 import requests
 from datetime import date
 import calendar
-import json
+import csv
+import io
 import boto3
 
-def pegar_historico_mensal_json_enviar_s3(latitude, longitude, bucket_name, s3_key):
+def pegar_historico_mensal_csv_enviar_s3(latitude, longitude, bucket_name, s3_key):
     hoje = date.today()
     ano = hoje.year
     mes_atual = hoje.month
@@ -36,38 +37,34 @@ def pegar_historico_mensal_json_enviar_s3(latitude, longitude, bucket_name, s3_k
         for i in range(len(dias)):
             minima = temp_min[i]
             maxima = temp_max[i]
-            
-            if minima is not None and maxima is not None:
-                media = round((minima + maxima) / 2, 1)
-            else:
-                media = "NULL"
+            media = round((minima + maxima) / 2, 1) if minima is not None and maxima is not None else "NULL"
 
-            historico.append({
-                "data": dias[i],
-                "temperatura_minima": minima,
-                "temperatura_maxima": maxima,
-                "temperatura_media": media
-            })
+            historico.append([dias[i], minima, maxima, media])
 
-    json_conteudo = json.dumps(historico, indent=4, ensure_ascii=False)
+    # Criar conteúdo CSV usando StringIO
+    csv_buffer = io.StringIO()
+    writer = csv.writer(csv_buffer)
+    writer.writerow(['data', 'temperatura_minima', 'temperatura_maxima', 'temperatura_media'])
+    writer.writerows(historico)
 
+    # Enviar CSV para o S3
     s3 = boto3.client('s3')
     try:
         s3.put_object(
             Bucket=bucket_name,
             Key=s3_key,
-            Body=json_conteudo.encode('utf-8'),
-            ContentType='application/json'
+            Body=csv_buffer.getvalue().encode('utf-8'),
+            ContentType='text/csv'
         )
-        print(f"✅ JSON enviado com sucesso para s3://{bucket_name}/{s3_key}")
+        print(f"✅ CSV enviado com sucesso para s3://{bucket_name}/{s3_key}")
     except Exception as e:
         print(f"Erro ao enviar para o S3: {e}")
 
 
 # bucket_name: substitua pelo nome do seu bucket
-pegar_historico_mensal_json_enviar_s3(
+pegar_historico_mensal_csv_enviar_s3(
     latitude=-23.55,
     longitude=-46.63,
-    bucket_name='bucket-raw-upa-connect-gabriel',
-    s3_key='historico_climatempo_2025.json'
+    bucket_name='bucket-raw-upa-connect', # trocar pelo nome do seu bucket!!
+    s3_key='historico_climatempo_2025.csv'
 )
